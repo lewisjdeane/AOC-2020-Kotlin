7c441810de9b124fc63ce287dbb7b60160eeeaab
Lewis Deane <lewisjdeane@gmail.com>
2020-12-15 20:06:47 +0000

    Add some extensions

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,3 +1,13 @@
 fun <T, U : Any> List<T>.firstNotNull(transform: (T) -> U?): U? = this.mapNotNull { transform(it) }.first()
 
 fun List<Int>.product(): Long = this.fold(1L) { acc, i -> acc * i }
+fun String.fromBinary(): Long = this.toLong(2)
+fun Int.toBinary(): String = Integer.toBinaryString(this)
+fun Long.toPowerOf(l: Long): Long = if (l == 0L) 1L else this * this.toPowerOf(l - 1L)
+fun String.padToLength(length: Int): CharArray {
+    val copy = this.toMutableList()
+    while (copy.size < length) {
+        copy.add(0, '0')
+    }
+    return copy.toCharArray()
+}
