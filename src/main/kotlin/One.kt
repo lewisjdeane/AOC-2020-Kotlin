beb9a4c61a6b08a0d562b563d56f17a1d7149e7f
Lewis Deane <lewisjdeane@gmail.com>
2020-12-15 20:07:53 +0000

    Tidy

--- src/main/kotlin/One.kt
+++ src/main/kotlin/One.kt
@@ -4,9 +4,8 @@ class One : Day<Int?, Int>(1) {
 
     private val costs = super.inputLines.map { it.toInt() }
 
-    override fun a(): Int? = productForTarget()
-
-    override fun b(): Int = costs.mapNotNull { cost -> productForTarget(TARGET - cost)?.let { it * cost } }.first()
+    override fun a() = productForTarget()
+    override fun b() = costs.mapNotNull { cost -> productForTarget(TARGET - cost)?.let { it * cost } }.first()
 
     private fun productForTarget(target: Int = TARGET): Int? {
         return costs.find { cost -> target - cost in costs }?.let { it * (target - it) }
